132ee6b8b41d2a85219d6cc99f30a9278dbdf4ae
herikerbeth <herik.docarmo@hotmail.com>
2024-07-24 15:51:56 -0300

    Update Card model with Jpa

--- src/main/java/fin_service/domain/model/Card.java
+++ src/main/java/fin_service/domain/model/Card.java
@@ -1,11 +1,29 @@
 package fin_service.domain.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity(name = "tb_card")
 public class Card {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String number;
+
+    @Column(scale = 13, precision = 2)
     private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNumber() {
         return number;
