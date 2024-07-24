72e6ef3f1adef24c82441c2da83669fd2c3b5863
herikerbeth <herik.docarmo@hotmail.com>
2024-07-23 23:29:29 -0300

    Create User model

---
+++ src/main/java/fin_service/domain/model/User.java
@@ -0,0 +1,52 @@
+package fin_service.domain.model;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private Account account;
+    private List<Features> features;
+    private Card card;
+    private List<New> news;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<Features> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Features> features) {
+        this.features = features;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<New> getNews() {
+        return news;
+    }
+
+    public void setNews(List<New> news) {
+        this.news = news;
+    }
+}
