806fd39439c88925d1a9c4ffc47414f7a9fb125f
herikerbeth <herik.docarmo@hotmail.com>
2024-07-23 22:10:21 -0300

    first commit

---
+++ src/test/java/fin_service/FinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
