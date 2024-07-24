ccee84fee70a8e9deeb8924ca5fefc8ae6deeaf7
herikerbeth <herik.docarmo@hotmail.com>
2024-07-24 15:21:20 -0300

    Create Card model

---
+++ src/main/java/fin_service/domain/model/Card.java
@@ -0,0 +1,25 @@
+package fin_service.domain.model;
+
+import java.math.BigDecimal;
+
+public class Card {
+
+    private String number;
+    private BigDecimal limit;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
