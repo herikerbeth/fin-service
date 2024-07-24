453d71b9584989cf7e7cb805f584ef27b484b7a2
herikerbeth <herik.docarmo@hotmail.com>
2024-07-24 15:54:09 -0300

    Update Feature model with Jpa

--- src/main/java/fin_service/domain/model/Feature.java
+++ src/main/java/fin_service/domain/model/Feature.java
@@ -1,9 +1,28 @@
 package fin_service.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_feature")
 public class Feature {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String icon;
+
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getIcon() {
         return icon;
