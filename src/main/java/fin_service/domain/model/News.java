e63290360c567e03d9de9df7a88ca8840d2eaf57
herikerbeth <herik.docarmo@hotmail.com>
2024-07-24 15:55:51 -0300

    Update News model with Jpa

--- src/main/java/fin_service/domain/model/News.java
+++ src/main/java/fin_service/domain/model/News.java
@@ -1,9 +1,28 @@
 package fin_service.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_news")
 public class News {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String icon;
+
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getIcon() {
         return icon;
